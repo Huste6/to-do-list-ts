329a5d70debce11b77a575471fb4a7f0017e6fba
Huste6 <quannghihai1@gmail.com>
2024-08-02 16:33:30 +0700

    Lấy ra danh sách công việc

---
+++ models/task.model.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema(
+    {   
+        title:String,
+        status:String,
+        content:String,
+        timeStart:Date,
+        timeFinish: Date,
+        createdBy:String,
+        listUser:Array,
+        TaskParentId:String,
+        deleted:{
+            type: Boolean,
+            default: false
+        },
+        deletedAt: Date,
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Task = mongoose.model("Task", TaskSchema, "tasks");
+
+export default Task;
