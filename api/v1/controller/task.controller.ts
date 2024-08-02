3627709eb6556f75a962d8ca83487af67f9acae0
Huste6 <quannghihai1@gmail.com>
2024-08-02 20:54:01 +0700

    sắp xếp + bộ lọc

--- api/v1/controller/task.controller.ts
+++ api/v1/controller/task.controller.ts
@@ -3,9 +3,27 @@ import Task from "../models/task.model";
 
 //[GET] /api/v1/tasks
 export const index = async (req: Request,res: Response) => {
-    const tasks = await Task.find({
+    //find
+    interface Find{
+        deleted:boolean,
+        status?:string
+    }
+    const find:Find = {
         deleted:false
-    })
+    }
+    if(req.query.status){
+        find.status = req.query.status.toString();
+    }
+    //end find
+    
+    //sort
+    const sort = {};
+    if(req.query.sortKey && req.query.sortValue){
+        const sortKey = req.query.sortKey.toString();
+        sort[sortKey]=req.query.sortValue;
+    }
+    //end sort
+    const tasks = await Task.find(find).sort(sort);
     res.json({
         code:200,
         tasks: tasks
