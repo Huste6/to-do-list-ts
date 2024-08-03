84c0e8008a115249e279e65b911027f11d81d5ec
Huste6 <quannghihai1@gmail.com>
2024-08-03 08:41:53 +0700

    login

--- api/v1/routes/users.route.ts
+++ api/v1/routes/users.route.ts
@@ -4,4 +4,6 @@ import * as controller from "../controller/users.controller"
 
 router.post("/register",controller.register)
 
+router.post("/login",controller.login)
+
 export const userRoutes:Router = router; 
